4e93e3b5f5ce7bccdc076c8f89152ac8061f8906
Wonhee Yoon <wonhee12@yonsei.ac.kr>
2021-03-24 22:32:58 +0900

    finished week4 pandas

--- 4주차/prac.py
+++ 4주차/prac.py
@@ -41,12 +41,6 @@ dropped_data = data.dropna(
 )
 data
 
-dropped_data = data.dropna(
-    axis='rows', 
-    how='any',
-    # inplace=True
-    )
-
 dropped_data = dropped_data.reset_index()
 dropped_data
 
@@ -59,6 +53,46 @@ dropped_data['rt'][3:5]
 dropped_data.iloc[3:5, 2]
 
 
+
+
+
+# import random
+# import numpy as np
+# import scipy as sp
+# import scipy.stats as stats
+
+# data2 = pd.DataFrame()
+# data
+
+# data2['sn'] = np.repeat(2, len(data))
+# zeros = np.zeros(15)
+# ones = np.ones(len(data) - len(zeros))
+
+# zeros.shape
+# ones.shape
+# corr = np.concatenate([zeros, ones])
+# random.shuffle(corr)
+
+# data2['cor'] = corr
+
+# rt2 = population.rvs(size=len(data))
+# data2['rt'] = np.abs(data['rt'] + rt2)
+
+# data2.describe()
+
+# data2.to_csv(
+#     'pandas_practice2.csv', 
+#     index=False
+#     )
+
+# data2 = pd.read_csv('pandas_practice2.csv')
+# data2.shape
+# data2
+
+data = pd.concat([data, data2])
+data.describe()
+data.groupby('sn').describe()
+
 data
 data.dropna()
 
@@ -70,6 +104,6 @@ dropped_data['rt'][3:5]
 
 
 
-
 dropped_data.describe()
 
+data.describe()
